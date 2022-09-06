6da66561bb6fb6309ffb9d19a3d20a0ca241b436
Faradius <faradius.magic187@gmail.com>
2022-09-06 00:43:42 -0500

    Finalización de la documentación del funcionamiento de la arquitectura del MVP

--- app/src/main/java/com/anncode/offersandcoupons/model/CouponRepository.kt
+++ app/src/main/java/com/anncode/offersandcoupons/model/CouponRepository.kt
@@ -2,5 +2,6 @@ package com.anncode.offersandcoupons.model
 
 interface CouponRepository {
 
+    //Es el metodo que se implementará en el repositorio para traer los cupones mediante el api
     fun getCouponsAPI()
 }
